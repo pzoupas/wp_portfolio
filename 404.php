8999bab3b3b173de756cfbf456ab45f464b91d07
zoupas.p <pzoupas29@gmail.com>
2023-11-09 00:40:09 +0200

    create 404 page

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php
+    get_header();
+?>
+
+<section class="error-page">
+    <div class="container">
+        <div class="error-page-wrapper">
+            <div class="slim-title">Η ΣΕΛΙΔΑ ΔΕΝ ΒΡΕΘΗΚΕ</div>
+            <h1 class="heading">Error<span>404</span></h1>
+            <div class="text">Λυπούμαστε, αλλά η σελίδα αυτή δεν υπάρχει στον ιστότοπο μας.</div>
+            <a href=""> <?php get_home_url() ?>  </a>
+        </div>
+    </div>
+</section>
