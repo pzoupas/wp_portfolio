0b7d3fa4a24bacff2fd91597d5f618955dca4db1
zoupas.p <pzoupas29@gmail.com>
2023-11-30 12:20:23 +0200

    contact form

---
+++ components/contact-form.php
@@ -0,0 +1,21 @@
+<section class="contact-form" >
+    <div class="container">
+        <h2><?php the_field('title-form'); ?></h2>
+        <?php acf_form(array(
+            'post_id' => 'new_post',
+            'post_title' => false, // Default:true -> το αλλαζω σε false για να εξαφανιζω το title* 
+            'post_content' => false,
+            'submit_value' => 'Submit Form',
+            'fields' => array('name', 'email', 'object', 'summary'),
+        ));?>
+    </div>
+</section>
+<div class="popup d-none">
+    <div class="popupOverlay"></div>
+    <div class="popupBox">
+        <button>close</button>
+        <h3>Συγχαρητήρια</h3>
+        <p>Το μήνυμά σας εστάλη με επιτυχία</p>
+        <span></span>
+    </div>
+</div>
