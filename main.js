9b4f77100e7d820604136dc6005fc73a76d9301b
zoupas.p <pzoupas29@gmail.com>
2024-01-11 13:12:01 +0200

    delete  autoplay Sound(video) file

--- main.js
+++ main.js
@@ -12,14 +12,14 @@ $(document).ready(function() {
 
     };
 
-     /***** Scroll to next  ****/
-     $('.scroll_to_next').click (function() {
-        $('html, body').animate({scrollTop: $('.hero-banner + section').offset().top }, 500 ,'linear');
+    /***** Scroll to next  ****/
+    $('.scroll_to_next').click (function() {
+    $('html, body').animate({scrollTop: $('.hero-banner + section').offset().top }, 500 ,'linear');
         return false;
-      });
+    });
 
     /***** Fade Header When I scroll Down   ****/
-      $(window).scroll(function () {
+    $(window).scroll(function () {
         if ($(this).scrollTop() < 80) {
             $('.header').fadeIn(500); //500ms
         } else {
@@ -27,8 +27,33 @@ $(document).ready(function() {
         }
     });
 
+    /***** autoplay Sound(video)  ****/
+
+    var sound = $('.sound-file')[0];
+    var video = $('.video-upload')[0];
+    var videoTop = $('.video-file').offset().top;
+    var videoBottom = videoTop + $('.video-file').height();
+    var maxVolume = sound.volume;
+
+    // Ακούστε το συμβάν scroll και το συμβάν resize.
+    $(window).on('scroll resize', function() {
+        var windowHeight = $(window).height();
+        var windowTop = $(window).scrollTop();
+        var windowBottom = windowTop + windowHeight;
+
+        if (videoTop < windowBottom && videoBottom > windowTop) {
+            // Αν το video είναι εντός της οθόνης, ξεκινήστε τον ήχο.
+            sound.volume = maxVolume;
+            video.play(); // Ξεκινήστε το video.
+        } else {
+            // Όταν το video δεν είναι εντός της οθόνης, σταματήστε τον ήχο.
+            sound.volume = 0;
+            video.pause(); // Σταματήστε το video.
+        }
+    });
+
+  
+
+
     
-
-
-
 });
